e600dd369a3027c4eea6b49a6a180718609be891
Gus Caplan <me@gus.host>
2017-04-27 10:10:09 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const Gopher = require('./src');
 const server = new Gopher('localhost');
 
 server.route('/', (res) => {
-  res.writeItem('0', 'SOME DIRECTORY', '/');
+  res.writeItem('DIRECTORY', 'some random directory', '/that-directory');
   res.writeInfo('hi!');
   res.end();
 });
